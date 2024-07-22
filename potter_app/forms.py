477e880adba16eefb47953e104570c8c90b0f48c
unknown <tayeakinrele@gmail.com>
2024-07-22 18:19:22 +0100

    new changes

--- potter_app/forms.py
+++ potter_app/forms.py
@@ -6,10 +6,10 @@ from potter_app.models import Category, Events
 
 class RegisterForm(forms.Form):
 	name = forms.CharField(label='Enter Your Name', required=True, widget=forms.TextInput(
-		attrs={'class': 'form-group', 'placeholder': 'Bisi-Akinlabi Pemisire','type':'text',}))
+		attrs={'class': 'form-group', 'placeholder': 'Your Name','type':'text',}))
 	from_email = forms.EmailField(label="Enter Your Email", required=True, widget=forms.TextInput(
-		attrs={'class': 'form-group', 'placeholder': 'bisiakinlabi@youremail.com', 'type': 'email'}))
+		attrs={'class': 'form-group', 'placeholder': 'Your Email', 'type': 'email'}))
 	message = forms.CharField(label='Type Your Messages',required=True, widget=forms.Textarea(
-		attrs={'class': 'form-group', 'placeholder': 'i would like to talk to...'}))
+		attrs={'class': 'form-group', 'placeholder': 'I would like to talk to...'}))
 
 
